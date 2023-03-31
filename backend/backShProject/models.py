d6079bf30bd3483cbd4b0eaae7ac5a235d46e051
arkannjo <reinaldofirme@gmail.com>
2023-03-31 01:23:23 -0300

    pre criação Models

--- backend/backShProject/models.py
+++ backend/backShProject/models.py
@@ -1,3 +1,33 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=300, blank=True)
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    # outros campos necessários para o perfil do usuário
+
+class Community(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    members = models.ManyToManyField(User, related_name='communities_joined')
+    # outros campos necessários para a comunidade
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # outros campos necessários para o post
+
+class Feed(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    posts = models.ManyToManyField(Post, related_name='feeds')
+    # outros campos necessários para o feed do usuário
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages_sent')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages_received')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # outros campos necessários para a mensagem
