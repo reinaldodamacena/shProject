08b19dd45950883af729795f2fd79edf489726b6
arkannjo <reinaldofirme@gmail.com>
2023-05-10 09:48:02 -0300

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,3 +1,86 @@
+import axios from 'axios';
+import { LOGIN_ROUTE, POSTS_ROUTE, FEED_USER_ROUTE, PROFILE_ROUTE } from './apiRoutes';
+
 const api = axios.create({
-    baseURL: 'http://shproject-backend:8000',
-  });
+  baseURL: 'http://127.0.0.1:8000', // Altere a URL base da sua API conforme necessário
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const login = async (username, password) => {
+  try {
+    const response = await api.post(LOGIN_ROUTE, { username, password });
+    if (response.status === 200 && response.data && response.data.token) {
+      localStorage.setItem('authToken', response.data.token);
+      return response.data;
+    } else {
+      throw new Error('Erro de autenticação');
+    }
+  } catch (error) {
+    throw new Error('Erro de autenticação: ' + error.message);
+  }
+};
+
+
+export const createPost = async (formData) => {
+  try {
+    const authToken = localStorage.getItem('authToken');
+    const response = await api.post(POSTS_ROUTE, formData, {
+      headers: {
+        Authorization: `Token ${authToken}`,
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    if (response.status === 201) {
+      return response.data;
+    } else {
+      throw new Error('Erro ao criar a publicação');
+    }
+  } catch (error) {
+    throw new Error('Erro ao criar a publicação: ' + error.message);
+  }
+};
+
+export const getPosts = async () => {
+  try {
+    const response = await api.get(POSTS_ROUTE);
+    return response.data;
+  } catch (error) {
+    throw new Error('Erro ao obter posts');
+  }
+};
+
+export const getFeedData = async () => {
+  try {
+    const authToken = localStorage.getItem('authToken');
+    console.log(authToken)
+    const response = await api.get(FEED_USER_ROUTE, {
+      headers: {
+        Authorization: `Token ${authToken}`,
+      },
+    });
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      throw new Error('Erro ao obter o feed do usuário');
+    }
+  } catch (error) {
+    throw new Error('Erro ao obter o feed do usuário: ' + error.message);
+  }
+};
+
+export const getProfileData = async (authToken) => {
+  try {
+    const response = await api.get(PROFILE_ROUTE, {
+      headers: {
+        Authorization: `Token ${authToken}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error('Erro ao obter as informações do perfil: ' + error.message);
+  }
+};
