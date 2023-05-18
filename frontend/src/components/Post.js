59e303eb6a53512c0c8da95cad912b2b33f5a99e
cHIsIMun <lucas.oliv001@gmail.com>
2023-05-18 18:54:55 -0300

    Alterações

--- frontend/src/components/Post.js
+++ frontend/src/components/Post.js
@@ -5,14 +5,20 @@ import InsertionLikeIcon from '../icons/like.svg';
 import InsertionUnlikeIcon from '../icons/unlike.svg';
 import InsertionCommentIcon from '../icons/comment.svg';
 import InsertionSendIcon from '../icons/send.svg';
-import './Post.css'
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import { IconButton } from '@mui/material';
+import SendIcon from '@mui/icons-material/Send'
+import './Post.css';
 
-
-const Post = ({ user, profile, content, file, timestamp, likes, postId }) => {
+const Post = ({ user, profile, content, file, timestamp, likes, comments, postId }) => {
   const [postLikes, setPostLikes] = useState([]);
   const [liked, setLiked] = useState(false);
+  const [showComments, setShowComments] = useState(false);
+  const [newComment, setNewComment] = useState('');
+
   useEffect(() => {
-    const fetchPostLikes = async () => {  
+    const fetchPostLikes = async () => {
       try {
         const authToken = localStorage.getItem('authToken');
         const endpoint = `http://localhost:8000/posts/${postId}/check_like/`;
@@ -38,25 +44,23 @@ const Post = ({ user, profile, content, file, timestamp, likes, postId }) => {
     try {
       const authToken = localStorage.getItem('authToken');
       const endpoint = `http://localhost:8000/posts/${postId}/like/`;
-  
-      const newLikedState = !liked; // Novo estado do liked
-  
-      // Atualiza o estado do liked imediatamente
+
+      const newLikedState = !liked;
+
       setLiked(newLikedState);
-  
+
       const response = await axios.post(endpoint, null, {
         headers: {
           'Authorization': `Token ${authToken}`,
         },
       });
-  
+
       if (response.status === 200) {
-        // Atualize o estado postLikes, se necessário
         const newLike = { id: user.id };
         if (newLikedState) {
           setPostLikes([...postLikes, newLike]);
         } else {
-          const updatedLikes = postLikes.filter(like => like.id !== user.id);
+          const updatedLikes = postLikes.filter((like) => like.id !== user.id);
           setPostLikes(updatedLikes);
         }
       } else {
@@ -67,42 +71,91 @@ const Post = ({ user, profile, content, file, timestamp, likes, postId }) => {
     }
   };
 
+  const handleCommentSubmit = (event) => {
+    event.preventDefault();
+    // Lógica para enviar o novo comentário para o backend e atualizar a lista de comentários
+    // ...
+    const newCommentObject = {
+      id: Date.now(), // Ou outro identificador único para o comentário
+      text: newComment,
+      // Outras informações do comentário, como autor, data, etc.
+    };
+    setShowComments([...comments, newCommentObject]);
+    //setNewComment('');
+  };
+
   return (
     <div className="post">
       <div className="head-post">
-        {console.log(profile.user)}
-        <Avatar alt={profile.user.username} src={profile.avatar} sx={{ width: 56, height: 56 }}/>
+        <Avatar alt={profile.user.username} src={profile.avatar} sx={{ width: 56, height: 56 }} />
         <div className="post-info">
-        <h3 className="post-author">{profile.user.first_name}</h3>
-        <p className="post-data">{new Date(timestamp).toLocaleDateString('pt-BR', {
-        day: 'numeric', 
-        month: 'long', 
-        year: 'numeric'
-        })}</p></div>
+          <h3 className="post-author">{profile.user.first_name}</h3>
+          <p className="post-data">
+            {new Date(timestamp).toLocaleDateString('pt-BR', {
+              day: 'numeric',
+              month: 'long',
+              year: 'numeric',
+            })}
+          </p>
+        </div>
       </div>
       <p className="post-content">{content}</p>
 
       {file && (
         <div className="post-media">
-          {file.endsWith('.mp4') ? (
+          {file && file.endsWith('.mp4') ? (
             <video className="post-video" controls controlsList="nodownload">
-              <source src={file} type="video/mp4" />
-              Seu navegador não suporta reprodução de vídeo.
-            </video>
-          ) : (
-            <img className="post-img" src={file} alt="Post Image" />
-          )}
-        </div>
-      )}
+            <source src={file} type="video/mp4" />
+            Seu navegador não suporta reprodução de vídeo.
+          </video>
+        ) : (
+          <img className="post-img" src={file} alt="Post Image" />
+        )}
+      </div>
+    )}
 
-      {/* Opções "curtir", "comentar" e "compartilhar" */}
-      <div className="b-opitions">
-        <div className="b-like" onClick={handleLike}>{liked ? <img src={InsertionLikeIcon} alt="Descurtir"/> : <img src={InsertionUnlikeIcon} alt="Curtir"/>} {}</div>
-        <div className="b-comment"><img src={InsertionCommentIcon} alt="Comentar"/></div>
-        <div className="b-send"><img src={InsertionSendIcon} alt="Compartilhar"/></div>
+    <div className="b-opitions">
+      <div className="b-like" onClick={handleLike}>
+        {liked ? (
+          <img src={InsertionLikeIcon} alt="Descurtir" />
+        ) : (
+          <img src={InsertionUnlikeIcon} alt="Curtir" />
+        )}
+      </div>
+      <div className="b-comment" onClick={() => setShowComments(!showComments)}>
+        <img src={InsertionCommentIcon} alt="Comentar" />
+      </div>
+      <div className="b-send">
+        <img src={InsertionSendIcon} alt="Compartilhar" />
       </div>
     </div>
-  );
+
+    {showComments && (
+      <div className="comments-section">
+        {comments.map((comment) => (
+          <div key={comment.id} className="comment">
+            <b>{comment.author.first_name}</b>
+            <p>{comment.content}</p>
+          </div>
+        ))}
+
+        <form onSubmit={handleCommentSubmit}>
+        <Box className="message-input">
+          <Input
+            placeholder="Digite sua mensagem" 
+            variant="soft"
+            endDecorator={IconButton}
+          />
+          <IconButton className='send-icon' color="background: #00000099;">
+            <SendIcon className='icon-s' />
+          </IconButton>
+        </Box>
+        </form>
+      </div>
+    )}
+  </div>
+);
 };
 
 export default Post;
+
